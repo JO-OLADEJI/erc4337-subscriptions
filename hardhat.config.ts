5633707c5554602ac736a87bc1ac99635073f6e5
analogdev.eth <dejijolaoluwa@gmail.com>
2024-01-09 01:32:31 +0100

    add deploy script & addresses

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      chainId: 80001,
+      accounts: [process.env.EOA_SIGNER ?? ""],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,42 @@
 import { ethers } from "hardhat";
+import deployConfig from "../deploy-config.json";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
+const deployPayModule = async () => {
+  try {
+    const PayModule = await ethers.deployContract("RecurringPaymentsModule");
+    await PayModule.waitForDeployment();
+    console.log("RecurringPaymentsModule: ", await PayModule.getAddress());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deploySubRouter = async () => {
+  try {
+    const SUBSCRIPTION_PRICE = ethers.parseEther("0.01");
+    const SubRouter = await ethers.deployContract("SubscriptionRouter", [
+      SUBSCRIPTION_PRICE,
+      deployConfig.SUB_ROUTER_DEFAULT_OPERATOR === ""
+        ? ethers.ZeroAddress
+        : deployConfig.SUB_ROUTER_DEFAULT_OPERATOR,
+    ]);
+    await SubRouter.waitForDeployment();
+    console.log("SubscriptionRouter: ", await SubRouter.getAddress());
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 async function main() {
-  
+  if (process.env.EOA_SIGNER === undefined) {
+    throw new Error("EOA_SIGNER not found in .env");
+  }
+  await deployPayModule();
+  await deploySubRouter();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
